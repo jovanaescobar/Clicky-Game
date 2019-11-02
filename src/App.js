5b5511804ba37cc842bcd96970459d025a207933
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-02 19:53:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
-// import Donuts from "./components/Donuts";
-// import Wrapper from "./components/Wrapper";
-// import Score from "src/components/Score/Score.js";
-// import sweets from "./donuts.json";
-import "./App.css";
+import Donuts from "./components/Donuts";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import sweets from "./donuts.json";
+
+
+class App extends Component {
+  // Setting this.state.donuts to the donuts json array
+  state = {
+    sweets,
+    clickedDonutId: [],
+    total: 0,
+    goal: 8
+  };
 
 
 
+ // Map over this.state.donuts and render a donut card component for each donut object
+  render() {
+    return (
+      <div className="App">
 
+      <Wrapper>
+        <Score total={this.state.total}
+        goal={8} />
+        
+        {this.state.sweets.map(donut => (
+          <Donuts
+            shuffleDonuts={this.shuffleDonuts}
+            id={donut.id}
+            key={donut.id}
+            image={donut.image}
+         
+          />
+        ))}
+      </Wrapper>
 
+      </div>
+    );
+  }
+}
 
 export default App;
