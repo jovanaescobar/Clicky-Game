f45471f472dbdd0dd45b686cf9dae0cbb84be585
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-02 19:46:29 -0400

    Update Donuts.js

--- src/components/Donuts/Donuts.js
+++ src/components/Donuts/Donuts.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Donuts.css";
 
-
-
-
-
-
-
-
+function Donuts(props) {
+    return (
+      <div className="donuts img-container">
+            <img alt={props.name} src={props.image} id={props.id}/>
+            </div>    
+    );
+  }
 
 export default Donuts;
