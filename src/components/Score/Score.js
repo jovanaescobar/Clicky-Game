eb93f4f37c06e7df95dbd1514931de7401aea1eb
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-02 21:16:04 -0400

    Update Score.js

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -2,14 +2,7 @@ import React from "react";
 import "./Score.css";
 
 
-<<<<<<< HEAD
-
 function Score (props) {
-
-
-=======
-function Score (props) {
->>>>>>> e7b23b1e178e9ad295ee001faddda25322846086
     return <div className="gameStart">
       <h1 className="title">DONUTS! {props.title}</h1>
       <h4 className="rules">CLICK A DONUT TO BEGIN {props.rules}</h4>
@@ -18,10 +11,7 @@ function Score (props) {
     </div>
 };
 
-<<<<<<< HEAD
 
 
 export default Score;
-=======
-export default Score;
->>>>>>> e7b23b1e178e9ad295ee001faddda25322846086
+
