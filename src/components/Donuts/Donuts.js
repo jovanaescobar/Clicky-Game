135a41717600298270ca717bb10d277da56becfb
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-02 21:25:50 -0400

    Update Donuts.js

--- src/components/Donuts/Donuts.js
+++ src/components/Donuts/Donuts.js
@@ -15,13 +15,4 @@ function Donuts(props) {
 
 
 export default Donuts;
-function Donuts(props) {
-    return (
-      <div className="donuts img-container">
-           <img alt={props.name} src={props.image} id={props.id}
-            onClick={() => props.shuffleDonuts(props.id)} className='shuffleDonuts' />
-            </div>    
-    );
-  }
 
-export default Donuts;
