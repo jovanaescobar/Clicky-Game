e7b23b1e178e9ad295ee001faddda25322846086
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-02 20:00:51 -0400

    Update Donuts.js

--- src/components/Donuts/Donuts.js
+++ src/components/Donuts/Donuts.js
@@ -4,7 +4,8 @@ import "./Donuts.css";
 function Donuts(props) {
     return (
       <div className="donuts img-container">
-            <img alt={props.name} src={props.image} id={props.id}/>
+           <img alt={props.name} src={props.image} id={props.id}
+            onClick={() => props.shuffleDonuts(props.id)} className='shuffleDonuts' />
             </div>    
     );
   }
